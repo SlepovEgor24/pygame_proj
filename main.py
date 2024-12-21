fd67860d1701f588081b1cf86a7f41d1ccc96c5e
SlepovEgor <slepovstarsiy@gmail.com>
2024-12-21 23:06:34 +0300

    2player + world + trees + cutting 21.12.2024

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import random
 import pygame
 
 pygame.init()
-pygame.display.set_caption('survive')
+pygame.display.set_caption('survival')
 size = width, height = 1280, 720
 pixsel = height // 720
 screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
+pygame.font.init()
+path = pygame.font.match_font("arial")
+Font = pygame.font.Font(path, 25)
 
 
-def load_image(name, puth, colorkey=None):
+def load_image(name, puth, colorkey=None): ##Загрузка изображений
     fullname = os.path.join(f'files\\world{puth}', name)
     if not os.path.isfile(fullname):
         print(f'Файл с изображение "{fullname}" отсутствует')
@@ -29,10 +32,10 @@ def load_image(name, puth, colorkey=None):
 
 class Map:
     def __init__(self):
-        self.x = 2000
-        self.y = 2000
+        self.x = 5000
+        self.y = 5000
 
-class Player(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite): ##Класс игрока
     image = load_image("forester.png", "\\player")
 
     def __init__(self, *group):
@@ -73,7 +76,7 @@ class Player(pygame.sprite.Sprite):
             self.x -= v // FPS
 
 
-class Blocks(pygame.sprite.Sprite):
+class Blocks(pygame.sprite.Sprite): ## класс блока - елки
     image = load_image("tree.png", "\\blocks\\tree")
     image2 = load_image("stump.png", "\\blocks\\tree")
 
@@ -84,26 +87,28 @@ class Blocks(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (200 * pixsel, 200 * pixsel))
         self.rect = self.image.get_rect()
         w, h = self.rect.w, self.rect.h
-        self.x = random.randrange(int(-0.5 * map.x) + w, int(0.5 * map.x) - w)
-        self.y = random.randrange(int(-0.5 * map.y) + h, int(0.5 * map.y) - h)
+        self.x = random.randrange(int(-0.5 * map.x), int(0.5 * map.x))
+        self.y = random.randrange(int(-0.5 * map.y), int(0.5 * map.y))
 
     def update(self, *args):
         self.rect.x = self.x - player.x + 300 * pixsel
         self.rect.y = self.y - player.y - 200 * pixsel
+        #if (self.rect.x + self.rect.w < 0 or self.rect.x - self.rect.w > 1280 * pixsel or
+                #self.rect.y + self.rect.h < 0 or self.rect.y - self.rect.h > 720 * pixsel):
         if abs(self.rect.x - 640 * pixsel + 50) < 30 * pixsel and abs(self.rect.y - 360 * pixsel + 100) < 30 * pixsel:
             self.image = Blocks.image2
             self.image = pygame.transform.scale(self.image, (200 * pixsel, 200 * pixsel))
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': ## Начало работы программы
     running = True
     map = Map()
-    for i in range(80):
+    for i in range(800):
         Blocks(all_sprites)
     player = Player(all_sprites)
-    FPS = 50
-    v = 50
+    FPS = 100
+    v = 500
     clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
@@ -115,9 +120,10 @@ if __name__ == '__main__':
                 all_sprites.update(event)
             if event.type == pygame.KEYUP:
                 all_sprites.update(event)
-        screen.fill((0, 0, 0))
+        screen.fill((100, 100, 100))
         clock.tick(FPS)
         all_sprites.draw(screen)
         all_sprites.update()
+        screen.blit(Font.render(f'x: {player.x} y: {player.y}', 1, (255, 255, 255)), (10, 10))
         pygame.display.flip()
     pygame.quit()
