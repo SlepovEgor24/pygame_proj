f566159383667a61f1e30df3a645f5e01a5b106f
SlepovEgor <slepovstarsiy@gmail.com>
2024-12-22 12:25:10 +0300

    0.1.1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import random
 import pygame
 
+difficult = [8, 5]
 pygame.init()
 pygame.display.set_caption('survival')
 size = width, height = 1280, 720
@@ -12,6 +13,8 @@ all_sprites = pygame.sprite.Group()
 pygame.font.init()
 path = pygame.font.match_font("arial")
 Font = pygame.font.Font(path, 25)
+BONFIREEVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(BONFIREEVENT, difficult[1] * 1000)
 
 
 def load_image(name, puth, colorkey=None): ##Загрузка изображений
@@ -54,6 +57,7 @@ class Player(pygame.sprite.Sprite): ##Класс игрока
         self.x, self.y = 0, 0
         self.rect.x = width // 2
         self.rect.y = height // 2
+        self.count = False
 
     def update(self, *args):
         if args and args[0].type == pygame.KEYUP and pygame.key.name(args[0].key) == 'w':
@@ -107,16 +111,70 @@ class Blocks(pygame.sprite.Sprite):    ##класс блока - елки
                 abs(self.rect.y - 180 * pixsel + 100 * pixsel) < 30 * pixsel):
             self.image = Blocks.image2
             self.image = pygame.transform.scale(self.image, (200 * pixsel, 200 * pixsel))
+            player.count = True
+
+
+class Bonfire(pygame.sprite.Sprite):
+    image_0 = load_image("bonfire0.png", "\\blocks\\bonfire")
+    image_0 = pygame.transform.scale(image_0, (30 * pixsel, 30 * pixsel))
+    image_1 = load_image("bonfire1.png", "\\blocks\\bonfire")
+    image_1 = pygame.transform.scale(image_1, (30 * pixsel, 30 * pixsel))
+    image_2 = load_image("bonfire2.png", "\\blocks\\bonfire")
+    image_2 = pygame.transform.scale(image_2, (30 * pixsel, 30 * pixsel))
+    image_3 = load_image("bonfire3.png", "\\blocks\\bonfire")
+    image_3 = pygame.transform.scale(image_3, (30 * pixsel, 30 * pixsel))
+    image_4 = load_image("bonfire4.png", "\\blocks\\bonfire")
+    image_4 = pygame.transform.scale(image_4, (30 * pixsel, 30 * pixsel))
+    image_5 = load_image("bonfire5.png", "\\blocks\\bonfire")
+    image_5 = pygame.transform.scale(image_5, (30 * pixsel, 30 * pixsel))
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        global pixsel
+        self.image = Bonfire.image_3
+        self.rect = self.image.get_rect()
+        self.run = 3
+        self.x, self.y = 0, 0
+
+    def update(self, *args):
+        global BONFIREEVENT
+        self.rect.x = self.x - player.x + 300 * pixsel
+        self.rect.y = self.y - player.y - 200 * pixsel
+        watch = False
+        if (args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos) and player.count
+                and abs(self.rect.x - 320 * pixsel) < 30 * pixsel and
+                abs(self.rect.y - 180 * pixsel) < 30 * pixsel and self.run < 5):
+            player.count = False
+            self.run = min(self.run + 1, 5)
+            watch = True
+        if args and args[0].type == BONFIREEVENT:
+            self.run = max(self.run - 1, 0)
+            watch = True
+        if watch:
+            if self.run == 0:
+                self.image = Bonfire.image_0
+            elif self.run == 1:
+                self.image = Bonfire.image_1
+            elif self.run == 2:
+                self.image = Bonfire.image_2
+            elif self.run == 3:
+                self.image = Bonfire.image_3
+            elif self.run == 4:
+                self.image = Bonfire.image_4
+            elif self.run == 5:
+                self.image = Bonfire.image_5
 
 
 ##Начало работы программы
 if __name__ == '__main__':
     running = True
+    time = 0
     map = Map()
-    for i in range(800):
+    for i in range(difficult[0] * 100):
         Blocks(all_sprites)
     player = Player(all_sprites)
     print(*all_sprites)
+    bonfire = Bonfire(all_sprites)
     FPS = 100
     v = 500
     clock = pygame.time.Clock()
@@ -130,10 +188,14 @@ if __name__ == '__main__':
                 all_sprites.update(event)
             if event.type == pygame.KEYUP:
                 all_sprites.update(event)
+            if event.type == BONFIREEVENT:
+                all_sprites.update(event)
         screen.fill((100, 100, 100))
         clock.tick(FPS)
         all_sprites.draw(screen)
         all_sprites.update()
         screen.blit(Font.render(f'x: {player.x} y: {player.y}', 1, (255, 255, 255)), (10, 10))
+        time += 1
+        pygame.display.set_caption(f'survival {time // FPS // 60}:{time//FPS % 60}')
         pygame.display.flip()
     pygame.quit()
